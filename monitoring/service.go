f4a4df06d902ea517849f9829c96970b60277ccd
Kian Ostad <c@kianostad.com>
2021-01-08 20:56:07 +0330

    fix mem usage

--- monitoring/service.go
+++ monitoring/service.go
@@ -26,11 +26,15 @@ func memoryUsage(ctx context.Context) {
 		case <-tick:
 			var m runtime.MemStats
 			runtime.ReadMemStats(&m)
-			mem.Set(float64(m.Sys))
+			mem.Set(float64(bToMb(m.Sys)))
 		case <-ctx.Done():
 			return
 		}
 	}
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
 
 func Namespace() string {
