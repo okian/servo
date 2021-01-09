9adf13eaaa3e8985162b28f16d77a76afcc89505
Kian Ostad <c@kianostad.com>
2021-01-09 13:09:09 +0330

    update monitoring for db

--- db/monitoring.go
+++ db/monitoring.go
@@ -11,35 +11,35 @@ import (
 )
 
 var (
-	metricOpenConn  *prometheus.GaugeVec
-	metricInuseConn *prometheus.GaugeVec
-	metricIdleConn  *prometheus.GaugeVec
-	metricTotalWait *prometheus.GaugeVec
+	metricOpenConn  *prometheus.SummaryVec
+	metricInuseConn *prometheus.SummaryVec
+	metricIdleConn  *prometheus.SummaryVec
+	metricTotalWait *prometheus.SummaryVec
 )
 
 func metrics() {
-	metricOpenConn = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	metricOpenConn = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: monitoring.Namespace(),
 		Subsystem: "db",
 		Name:      "open_conn",
 	}, []string{
 		"host",
 	})
-	metricInuseConn = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	metricInuseConn = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: monitoring.Namespace(),
 		Subsystem: "db",
 		Name:      "inuse_conn",
 	}, []string{
 		"host",
 	})
-	metricIdleConn = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	metricIdleConn = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: monitoring.Namespace(),
 		Subsystem: "db",
 		Name:      "idle_conn",
 	}, []string{
 		"host",
 	})
-	metricTotalWait = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	metricTotalWait = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: monitoring.Namespace(),
 		Subsystem: "db",
 		Name:      "total_wait",
@@ -56,10 +56,10 @@ func monitor(ctx context.Context, d *sqlx.DB, host string) {
 		select {
 		case <-t:
 			st := d.Stats()
-			metricOpenConn.WithLabelValues(host).Sub(float64(st.OpenConnections))
-			metricInuseConn.WithLabelValues(host).Sub(float64(st.InUse))
-			metricIdleConn.WithLabelValues(host).Sub(float64(st.Idle))
-			metricTotalWait.WithLabelValues(host).Sub(float64(st.WaitDuration.Milliseconds()))
+			metricOpenConn.WithLabelValues(host).Observe(float64(st.OpenConnections))
+			metricInuseConn.WithLabelValues(host).Observe(float64(st.InUse))
+			metricIdleConn.WithLabelValues(host).Observe(float64(st.Idle))
+			metricTotalWait.WithLabelValues(host).Observe(float64(st.WaitDuration.Milliseconds()))
 		case <-ctx.Done():
 			return
 		}
